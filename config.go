7e30ade5db877e8462a16fcaf46ffd1993287b08
hugo <hugo@powder.gg>
2021-01-28 15:19:49 +0100

    add http client

--- config.go
+++ config.go
@@ -5,3 +5,8 @@ type Config struct {
 	Hostname string `json:"hostname" env:"HTTP_HOSTNAME" env-default:"0.0.0.0"`
 	Port     string `json:"port" env:"HTTP_PORT" env-default:"8080"`
 }
+
+// ClientConfig is http service config.
+type ClientConfig struct {
+	Domain string `json:"domain" env:"HTTP_CLIENT_DOMAIN"`
+}
