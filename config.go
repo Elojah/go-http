1cff766dd69ee3dd9b3d042388605c9ac53d2930
hdezier <hugo.dezier@platform.sh>
2022-10-19 20:52:12 +0200

    add TLS config keys

--- config.go
+++ config.go
@@ -5,6 +5,8 @@ type Config struct {
 	Hostname    string   `json:"hostname" env:"HTTP_HOSTNAME" env-default:"0.0.0.0"`
 	Port        string   `json:"port" env:"HTTP_PORT" env-default:"8080"`
 	AllowedCORS []string `json:"allowed_cors" env:"HTTP_ALLOWED_CORS"`
+	Cert        string   `json:"cert" env:"HTTP_CERT"`
+	Key         string   `json:"key" env:"HTTP_KEY"`
 }
 
 // ClientConfig is http service config.
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/elojah/go-http
 
-go 1.15
+go 1.19
 
 require (
 	github.com/gorilla/mux v1.8.0
