1b974045ba1ded8fd12b2d287726f5b4c8ba8e29
hdezier <hugo.dezier@platform.sh>
2022-10-20 15:13:10 +0200

    fix json name host

--- config.go
+++ config.go
@@ -5,7 +5,7 @@ type Config struct {
 	Hostname      string   `json:"hostname" env:"HTTP_HOSTNAME" env-default:"0.0.0.0"`
 	Port          string   `json:"port" env:"HTTP_PORT" env-default:"8080"`
 	AllowedCORS   []string `json:"allowed_cors" env:"HTTP_ALLOWED_CORS"`
-	HostWhitelist []string `json:"host_white_list" env:"HTTP_HOST_WHITELIST"`
+	HostWhitelist []string `json:"host_whitelist" env:"HTTP_HOST_WHITELIST"`
 }
 
 // ClientConfig is http service config.
